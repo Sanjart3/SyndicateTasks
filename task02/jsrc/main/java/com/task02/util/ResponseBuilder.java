923fc7e865a5b88d5bd1d67918d9cd73e4a760fd
Sanjar Totliboyev <sanjartotliboyev@gmail.com>
2024-10-23 09:47:56 +0500

    Some changes related the order of response

--- task02/jsrc/main/java/com/task02/util/ResponseBuilder.java
+++ task02/jsrc/main/java/com/task02/util/ResponseBuilder.java
@@ -1,11 +1,12 @@
 package com.task02.util;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class ResponseBuilder {
     public static Map<String, Object> build(int status, String message) {
-        Map<String, Object> result = new HashMap<>();
+        Map<String, Object> result = new LinkedHashMap<>();
         result.put("status", status);
         result.put("message", message);
         return result;
