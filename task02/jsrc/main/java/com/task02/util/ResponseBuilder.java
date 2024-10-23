1b367d1ca2504d1722fc051650d72165de931cc4
Sanjar Totliboyev <sanjartotliboyev@gmail.com>
2024-10-23 10:10:45 +0500

    back to Hashmap

--- task02/jsrc/main/java/com/task02/util/ResponseBuilder.java
+++ task02/jsrc/main/java/com/task02/util/ResponseBuilder.java
@@ -1,12 +1,11 @@
 package com.task02.util;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class ResponseBuilder {
     public static Map<String, Object> build(int status, String message) {
-        Map<String, Object> result = new LinkedHashMap<>();
+        Map<String, Object> result = new HashMap<>();
         result.put("statusCode", status);
         result.put("message", message);
         return result;
