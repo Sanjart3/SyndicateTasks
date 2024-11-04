e1027d554bc850bed03e43f13e47ab78f63c3996
Sanjar Totliboyev <sanjartotliboyev@gmail.com>
2024-11-04 17:53:58 +0500

    TASK05: fully restarted

--- task05/jsrc/main/java/com/task05/service/EventService.java
+++
@@ -1,9 +0,0 @@
-package com.task05.service;
-
-import com.task05.entity.Event;
-
-import java.util.Map;
-
-public interface EventService {
-    Event putEvent(int principalId, Map<String, Object> body);
-}
