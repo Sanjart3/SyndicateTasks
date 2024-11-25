ebe8ac99a3b09a7625b587e972b8187cbf90e913
Sanjar Totliboyev <sanjartotliboyev@gmail.com>
2024-11-25 08:03:35 +0500

    First try

---
+++ task10/jsrc/main/java/com/task10/handler/RouteHandler.java
@@ -0,0 +1,52 @@
+package com.task10.handler;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.task10.handler.impl.*;
+import com.task10.model.RouteKey;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class  RouteHandler {
+    private final Map<RouteKey, Handler> handlers = new HashMap<>();
+
+    public RouteHandler(CognitoIdentityProviderClient cognitoProvider) {
+
+        handlers.put(new RouteKey( "/signup", "POST"), new SignUpHandler(cognitoProvider));
+        handlers.put(new RouteKey( "/signin", "POST"), new SignInHandler(cognitoProvider));
+        handlers.put(new RouteKey( "/tables","POST"), new PostTableHandler());
+        handlers.put(new RouteKey( "/tables", "GET"), new GetTableHandler());
+        handlers.put(new RouteKey("/tables/{tableId}" ,"GET"), new GetTableByIdHandler());
+        handlers.put(new RouteKey( "/reservations", "POST"), new PostReservationHandler());
+        handlers.put(new RouteKey( "/reservations", "GET"), new GetReservationHandler());
+    }
+
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent) {
+        RouteKey routeKey = getRouteKey(requestEvent);
+        Handler handler = handlers.getOrDefault(routeKey, new RouteNotImplementedHandler());
+        return handler.handle(requestEvent);
+    }
+
+    private RouteKey getRouteKey(APIGatewayProxyRequestEvent requestEvent) {
+        String path = filterTableId(requestEvent);
+        return new RouteKey(path, requestEvent.getHttpMethod());
+    }
+
+    private static String filterTableId(APIGatewayProxyRequestEvent requestEvent) {
+        String path = requestEvent.getPath();
+        if (path.matches("/tables/\\d+")) {
+            path = "/tables/{tableId}";
+        }
+        return path;
+    }
+
+    private String getMethod(APIGatewayProxyRequestEvent requestEvent) {
+        return requestEvent.getHttpMethod();
+    }
+
+    private String getPath(APIGatewayProxyRequestEvent requestEvent) {
+        return requestEvent.getPath();
+    }
+}
---
+++ task10/jsrc/main/java/com/task10/model/SignIn.java
@@ -0,0 +1,19 @@
+package com.task10.model;
+
+import org.json.JSONObject;
+
+public record SignIn(String email, String password) {
+    public SignIn {
+        if (email == null || password == null) {
+            throw new IllegalArgumentException("Missing or incomplete data.");
+        }
+    }
+
+    public static SignIn fromJson(String jsonString) {
+        JSONObject json = new JSONObject(jsonString);
+        String nickName = json.optString("email", null);
+        String password = json.optString("password", null);
+
+        return new SignIn(nickName, password);
+    }
+}
